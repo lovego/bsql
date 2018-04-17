2d3ae605dc9353810afce5454d2a6c8f56eb4631
chenyun <yun.chen@hztl3.com>
2018-04-17 18:42:14 +0800

    写测试

--- utils_test.go
+++ utils_test.go
@@ -17,7 +17,7 @@ func TestQ(t *testing.T) {
 
 func TestColumn2Field(t *testing.T) {
 	var data = []string{"bsql_test", "xiao_mei", "love_go", "you123"}
-	var expect = []string{"PsqlTest", "XiaoMei", "LoveGo", "You123"}
+	var expect = []string{"BsqlTest", "XiaoMei", "LoveGo", "You123"}
 	for i := range data {
 		if got := Column2Field(data[i]); !reflect.DeepEqual(expect[i], got) {
 			t.Errorf("expect %v,got %v", expect, got)
@@ -26,7 +26,7 @@ func TestColumn2Field(t *testing.T) {
 }
 
 func TestColumn2Fields(t *testing.T) {
-	var expect = []string{"PsqlTest", "UserName", "Phone"}
+	var expect = []string{"BsqlTest", "UserName", "Phone"}
 	if got := Columns2Fields([]string{"bsql_test", "user_name", "phone"}); !reflect.DeepEqual(expect, got) {
 		t.Errorf("expect %v,got %v", expect, got)
 	}
@@ -38,8 +38,21 @@ func TestStructFieldsAddrs(t *testing.T) {
 		Name   string
 		Exists bool
 	}
-	addrs, err := StructFieldsAddrs(reflect.ValueOf(&bsqlTest).Elem(), []string{"Id", "Name", "Exists"})
-	if len(addrs) != 3 {
+	var addrs []interface{}
+	if addrs, err := StructFieldsAddrs(reflect.ValueOf(&bsqlTest).Elem(),
+		[]string{"Id", "Name", "Exists"}); err != nil || len(addrs) != 3 {
 		t.Fatalf("unexpted addrs size: %d", len(addrs))
+	}
+	for i := range addrs {
+		if _, ok := addrs[i].(*int64); ok {
+			continue
+		}
+		if _, ok := addrs[i].(*string); ok {
+			continue
+		}
+		if _, ok := addrs[i].(*bool); ok {
+			continue
+		}
+		t.Fatalf("指针类型错误", addrs[i])
 	}
 }
