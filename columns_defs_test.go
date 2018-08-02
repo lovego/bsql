229fcb5454fc5df958a6569e854f5ee59f93d99d
蔡俊杰 <junjie.cai@hztl3.com>
2018-08-02 15:40:34 +0800

    测试example输出

--- columns_defs_test.go
+++ columns_defs_test.go
@@ -35,5 +35,14 @@ func ExampleColumnsDefs() {
 		timeFields
 	}
 	got := ColumnsDefs(Student{})
-	fmt.Println(got)
+	// Output :
+	// `id serial8 not null primary key,
+	// name text not null,
+	// friend_ids int[] not null,
+	// cities jsonb not null,
+	// scores jsonb not null,
+	// money decimal not null,
+	// status int2 not null default 0,
+	// created_at timestamptz not null,
+	// updated_at timestamptz not null`
 }
