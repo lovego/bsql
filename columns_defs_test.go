507c98aaaddbaf50399253b83bd4162abde88b76
蔡俊杰 <junjie.cai@hztl3.com>
2018-08-02 16:18:28 +0800

    测试example

--- columns_defs_test.go
+++ columns_defs_test.go
@@ -1,25 +1,17 @@
 package bsql
 
-import (
-	"fmt"
-	"testing"
-)
+import "fmt"
 
-func ExampleColumnsDefs(t *testing.T) {
-	got := ColumnsDefs(Student{})
-	expect := `id serial8 not null primary key,
-name text not null,
-friend_ids int[] not null,
-cities jsonb not null,
-scores jsonb not null,
-money decimal not null,
-status int2 not null default 0,
-created_at timestamptz not null,
-updated_at timestamptz not null`
-	if got != expect {
-		t.Error("unexpetecd: ", got)
-	} else {
-		t.Log(got)
-	}
-	fmt.Println(expect)
+func ExampleColumnsDefs() {
+	fmt.Println(ColumnsDefs(Student{}))
+	// Output:
+	// id serial8 not null primary key,
+	// name text not null,
+	// friend_ids int[] not null,
+	// cities jsonb not null,
+	// scores jsonb not null,
+	// money decimal not null,
+	// status int2 not null default 0,
+	// created_at timestamptz not null,
+	// updated_at timestamptz not null
 }
