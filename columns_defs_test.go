63fa51d9ab5bf745aee697d02909ca37efc88f09
蔡俊杰 <junjie.cai@hztl3.com>
2018-08-02 15:46:34 +0800

    测试

--- columns_defs_test.go
+++ columns_defs_test.go
@@ -1,11 +1,10 @@
 package bsql
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestColumnsDefs(t *testing.T) {
+func ExampleColumnsDefs(t *testing.T) {
 	got := ColumnsDefs(Student{})
 	expect := `id serial8 not null primary key,
 name text not null,
@@ -21,28 +20,4 @@ updated_at timestamptz not null`
 	} else {
 		t.Log(got)
 	}
-}
-
-func ExampleColumnsDefs() {
-	type Student struct {
-		Id        int64
-		Name      string
-		FriendIds pq.Int64Array `sql:"int[]"`
-		Cities    []string
-		Scores    map[string]int
-		Money     decimal.Decimal
-		Status    int8 `sql:"default 0"`
-		timeFields
-	}
-	got := ColumnsDefs(Student{})
-	// Output :
-	// `id serial8 not null primary key,
-	// name text not null,
-	// friend_ids int[] not null,
-	// cities jsonb not null,
-	// scores jsonb not null,
-	// money decimal not null,
-	// status int2 not null default 0,
-	// created_at timestamptz not null,
-	// updated_at timestamptz not null`
 }
