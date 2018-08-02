47029260cd72b8350eeba1ebd6d947cf731030e7
蔡俊杰 <junjie.cai@hztl3.com>
2018-08-02 16:46:16 +0800

    完成第一个example

--- columns_defs_test.go
+++ columns_defs_test.go
@@ -1,8 +1,26 @@
 package bsql
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/lib/pq"
+	"github.com/shopspring/decimal"
+)
 
 func ExampleColumnsDefs() {
+	type Student struct {
+		Id        int64
+		Name      string
+		FriendIds pq.Int64Array `sql:"int[]"`
+		Cities    []string
+		Scores    map[string]int
+		Money     decimal.Decimal
+		Status    int8 `sql:"default 0"`
+		CreatedAt time.Time
+		UpdatedAt time.Time
+	}
+
 	fmt.Println(ColumnsDefs(Student{}))
 	// Output:
 	// id serial8 not null primary key,
