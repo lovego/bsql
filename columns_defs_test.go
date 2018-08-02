d9787657e37e7f10c5e8c664ef8c24ae012550cf
蔡俊杰 <junjie.cai@hztl3.com>
2018-08-02 15:52:59 +0800

    test

--- columns_defs_test.go
+++ columns_defs_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func ExampleColumnsDefs(t *testing.T) {
+func ExampleColumnsDefs(t *testing.InternalExample) {
 	got := ColumnsDefs(Student{})
 	expect := `id serial8 not null primary key,
 name text not null,
@@ -15,9 +15,10 @@ money decimal not null,
 status int2 not null default 0,
 created_at timestamptz not null,
 updated_at timestamptz not null`
-	if got != expect {
-		t.Error("unexpetecd: ", got)
-	} else {
-		t.Log(got)
-	}
+	// if got != expect {
+	// 	t.Error("unexpetecd: ", got)
+	// } else {
+	// 	t.Log(got)
+	// }
+	t.Output
 }
