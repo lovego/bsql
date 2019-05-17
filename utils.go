040b1ae0e6cd7e69f933bf51c45d70bf126d5286
bughou <applejava@qq.com>
2019-05-17 17:24:42 +0800

    add UpsertSql

--- utils.go
+++ utils.go
@@ -3,10 +3,48 @@ package bsql
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode/utf8"
 
 	"github.com/lib/pq"
 )
+
+func UpsertSql(table string, toInsert, conflictKeys, notToUpdate []string) string {
+	toInsert = Fields2Columns(toInsert)
+	conflictKeys = Fields2Columns(conflictKeys)
+	notToUpdate = Fields2Columns(notToUpdate)
+
+	// conflict keys should be inserted and not be updated.
+	for _, key := range conflictKeys {
+		if notIn(key, toInsert) {
+			toInsert = append(toInsert, key)
+		}
+		if notIn(key, notToUpdate) {
+			notToUpdate = append(notToUpdate, key)
+		}
+	}
+
+	toUpdate := make([]string, 0, len(toInsert))
+	excluded := make([]string, 0, len(toInsert))
+	for _, column := range toInsert {
+		if notIn(column, notToUpdate) {
+			excluded = append(excluded, "excluded."+column)
+			if column == "created_at" || column == "created_by" {
+				column = strings.Replace(column, "created", "updated", 1)
+			}
+			toUpdate = append(toUpdate, "         "+column)
+		}
+	}
+
+	return fmt.Sprintf(`INSERT INTO %s (%s)
+VALUES %%s
+ON CONFLICT (%s) DO UPDATE SET
+(%s) =
+(%s)`,
+		table, strings.Join(toInsert, ", "), strings.Join(conflictKeys, ", "),
+		strings.Join(toUpdate, ", "), strings.Join(excluded, ", "),
+	)
+}
 
 func ConflictedUniqueIndex(err error) string {
 	if pqError, ok := err.(*pq.Error); ok && pqError.Code == "23505" { // unique_violation
--- utils_test.go
+++ utils_test.go
@@ -2,6 +2,33 @@ package bsql
 
 import "fmt"
 
+func ExampleUpsertSql_1() {
+	fmt.Println(UpsertSql("users", []string{
+		"Phone", "Name", "Sex", "Birthday", "Status", "CreatedBy", "CreatedAt",
+	}, []string{"Phone"}, []string{"Status"},
+	))
+	// Output:
+	// INSERT INTO users (phone, name, sex, birthday, status, created_by, created_at)
+	// VALUES %s
+	// ON CONFLICT (phone) DO UPDATE SET
+	// (         name,          sex,          birthday,          updated_by,          updated_at) =
+	// (excluded.name, excluded.sex, excluded.birthday, excluded.created_by, excluded.created_at)
+}
+
+func ExampleUpsertSql_2() {
+	fmt.Println(UpsertSql(
+		"users", []string{
+			"phone", "name", "sex", "birthday", "Status", "created_by", "created_at",
+		}, []string{"phone"}, []string{"status"},
+	))
+	// Output:
+	// INSERT INTO users (phone, name, sex, birthday, status, created_by, created_at)
+	// VALUES %s
+	// ON CONFLICT (phone) DO UPDATE SET
+	// (         name,          sex,          birthday,          updated_by,          updated_at) =
+	// (excluded.name, excluded.sex, excluded.birthday, excluded.created_by, excluded.created_at)
+}
+
 func ExampleOffsetToLineAndColumn() {
 	fmt.Println(OffsetToLineAndColumn("", 0))
 	fmt.Println(OffsetToLineAndColumn("", 1))
