e872b31f9d4af94571ee8b6ef7eb53ca3c0b3b76
bughou <bughou@gmail.com>
2019-08-26 17:00:33 +0800

    use tracer.StartChild to avoid useless tracing

--- db.go
+++ db.go
@@ -44,7 +44,7 @@ func (db *DB) QueryT(duration time.Duration, data interface{}, sql string, args 
 func (db *DB) QueryCtx(ctx context.Context, opName string,
 	data interface{}, sql string, args ...interface{},
 ) error {
-	defer tracer.Finish(tracer.Start(ctx, opName))
+	defer tracer.Finish(tracer.StartChild(ctx, opName))
 	if ctx.Done() == nil {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, db.timeout)
@@ -84,7 +84,7 @@ func (db *DB) ExecT(duration time.Duration, sql string, args ...interface{}) (sq
 
 func (db *DB) ExecCtx(ctx context.Context, opName string,
 	sql string, args ...interface{}) (sql.Result, error) {
-	defer tracer.Finish(tracer.Start(ctx, opName))
+	defer tracer.Finish(tracer.StartChild(ctx, opName))
 	if ctx.Done() == nil {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, db.timeout)
@@ -122,7 +122,7 @@ func (db *DB) RunInTransactionT(duration time.Duration, fn func(*Tx) error) erro
 }
 
 func (db *DB) RunInTransactionCtx(ctx context.Context, opName string, fn func(*Tx, context.Context) error) error {
-	ctx = tracer.Start(ctx, opName)
+	ctx = tracer.StartChild(ctx, opName)
 	defer tracer.Finish(ctx)
 
 	if ctx.Done() == nil {
